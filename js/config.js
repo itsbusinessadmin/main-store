aaac42feba38ab2765b2db115aef38e10a6f7fa2
itsbusinessadmin <itsbusiness.admin@gmail.com>
2026-08-30 11:39:30 -0400

    Update Google Client ID in configuration

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.US_CONFIG = {
   APP_NAME: "Universal Store",
   API_BASE: "https://main-store.itsbusiness-admin.workers.dev",
   DEMO_MODE: false,
-  GOOGLE_CLIENT_ID: "YOUR_GOOGLE_OAUTH_CLIENT_ID.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "996370090328-3sukmrf88b4rtcj18dvdhmqulj11jcr3.apps.googleusercontent.com",
   CURRENCY: "PHP",
   CURRENCY_SYMBOL: "₱",
   PAGE_SIZE: 20,
